9885985a9a109e96fc9df5f950c6f8a85dadff4f
notNxl <121226665+notNxl@users.noreply.github.com>
2022-12-23 05:00:14 +1000

    Add files via upload

---
+++ Task2/Task2.cpp
@@ -0,0 +1,12 @@
+﻿#include <cstdio>
+#include <string>
+#include "Functions.h"
+
+int main()
+{
+	std::string a;
+	std::string s = input(a);
+	s = toUpper(s);
+	s.erase(std::remove_if(s.begin(), s.end(), isspace), s.end());
+	output(s);
+}
