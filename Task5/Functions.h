73ab9edad1ee217b3b2b163cba2eae339519f1c2
notNxl <121226665+notNxl@users.noreply.github.com>
2022-12-26 02:05:47 +1000

    Delete Task5 directory

--- Task5/Functions.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <atomic>
-#include <thread>
-
-void summarize(int n, int* inputArray, const int size, const int numberOfThreads, std::atomic<int>& sumResult);
-
-int* inputArr();
-
-int result(int numberOfThreads, int* inputArray);
