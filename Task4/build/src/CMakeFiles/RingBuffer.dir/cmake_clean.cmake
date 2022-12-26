5c5a249d129e2d601c533a5ec2f3533cefc78b34
notNxl <artemnxl1@gmail.com>
2022-12-26 11:37:04 +1000

    Task4

---
+++ Task4/build/src/CMakeFiles/RingBuffer.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/RingBuffer.dir/RingBuffer.cpp.o"
+  "CMakeFiles/RingBuffer.dir/RingBuffer.cpp.o.d"
+  "libRingBuffer.a"
+  "libRingBuffer.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/RingBuffer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Task4/build/src/CMakeFiles/RingBuffer.dir/cmake_clean_target.cmake
@@ -0,0 +1,3 @@
+file(REMOVE_RECURSE
+  "libRingBuffer.a"
+)
